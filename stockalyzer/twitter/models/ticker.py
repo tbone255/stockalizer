d176c68f6f1c4de52046efcbe01ca1a08444c1a1
Taha Amir <taha@Tahas-MacBook-Air.local>
2020-02-11 13:11:32 -0500

    Clean up files, start db models

---
+++ stockalyzer/twitter/models/ticker.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+class Ticker(models.Model):
+    symbol = models.CharField(max_length=100)
+    company = models.CharField(max_length=100)
---
+++ stockalyzer/twitter/models/trends.py
@@ -0,0 +1,9 @@
+from django.db import models
+from ./ticker import Ticker
+#from ./price_info import PriceInfo
+
+class Trend(models.Model):
+    ticker_id = models.ForeignKey(Tickers)
+    count = models.IntegerField();
+    #priceinfo_id = models.ForeignKey(PriceInfo)
+    date = models.DateField(auto_now_add=True)
