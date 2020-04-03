ebe6c1c65c21bbe4933993a5fd9ff8f06120793e
Ashish Singh <singh.ashish1998@gmail.com>
2020-04-02 22:21:40 -0400

    Using IEX Finance

--- stockalyzer/twitter/models/ticker.py
+++ stockalyzer/twitter/models/ticker.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+
 class Ticker(models.Model):
     symbol = models.CharField(max_length=100)
     company = models.CharField(max_length=100)
+    
+    
--- temp.py
+++ temp.py
@@ -1,3 +1,3 @@
-import pyEX
-symbol = "AAPL"
-thing = pyEX.stocks.profiles.company(symbol, token='pk_5e280dbbce2c454dad7da42f83c4b58f', version='v1', filter='')
+from iexfinance.stocks import Stock
+symbol = Stock("AAPL", token = "pk_5e280dbbce2c454dad7da42f83c4b58f")
+symbol.get_company()
