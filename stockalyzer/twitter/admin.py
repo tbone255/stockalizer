910441542578687eadbfab033a529e725405d783
FabioHux <huxfabio@gmail.com>
2020-04-07 20:57:49 -0400

    Renamed news_trends -> news_trend, register admin

--- stockalyzer/twitter/admin.py
+++ stockalyzer/twitter/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(News)
+admin.site.register(NewsTrend)
+admin.site.register(PriceInfo)
+admin.site.register(Ticker)
+admin.site.register(Trend)
