b301a2bf3c05a681385edd3ac965d6a38c98f84d
Taha Amir <taha.amir13971@gmail.com>
2020-04-24 19:48:28 +0000

    top 5 endpoint

--- twitter/viewsets/trend.py
+++ twitter/viewsets/trend.py
@@ -1,4 +1,7 @@
+from datetime import date
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from ..models import Trend
 from ..serializers import TrendSerializer
@@ -7,3 +10,11 @@ from ..serializers import TrendSerializer
 class TrendViewSet(viewsets.ModelViewSet):
     queryset = Trend.objects.all()
     serializer_class = TrendSerializer
+
+    @action(detail=False, methods=['get'])
+    def top(self, request):
+        yesterday = date.today()
+        yesterday.replace(day=yesterday.day-1)
+        trends = Trend.objects.filter(date=yesterday).order_by('count')[:5]
+        serializer = TrendSerializer(trends, many=True)
+        return Response(serializer.data)
