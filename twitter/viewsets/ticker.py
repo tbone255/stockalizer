24bb3b6c207881e9888cbeaa50e84315983a580b
Taha Amir <taha.amir13971@gmail.com>
2020-04-25 03:07:15 +0000

    Graph data endpoint

--- twitter/viewsets/ticker.py
+++ twitter/viewsets/ticker.py
@@ -3,6 +3,7 @@ import os
 from rest_framework import filters, viewsets 
 from rest_framework.decorators import action
 from rest_framework.response import Response
+import yfinance
 
 from ..models import NewsTrend, Ticker, Trend
 from ..serializers import NewsTrendNewsSerializer, BasicTrendSerializer, TickerSerializer
@@ -33,3 +34,14 @@ class TickerViewSet(viewsets.ModelViewSet):
     def price_info(self, request, pk=None):
         stock = Stock(self.get_object().symbol, token=os.getenv('IEX_TOKEN'))
         return Response(stock.get_key_stats())
+
+    @action(detail=True, methods=['get'])
+    def graph_data(self, request, pk=None):
+        symbol = self.get_object().symbol
+        ticker = yfinance.Ticker(symbol)
+        data = ticker.history('ytd')
+        data = data.reset_index()
+        data["Date"] = data['Date'].dt.date
+        data['Date'] = data['Date'].astype('str')
+        data = data.to_dict()
+        return Response(data)
